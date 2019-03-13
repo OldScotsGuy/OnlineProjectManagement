f4b5f9f16e9c396eb17714f16d1e8f87a9f0a961
Nick Harle <nickharle@gmail.com>
2019-03-13 16:43:22 +0000

    Minor updates to ProjectView

--- Objects/View/ProjectView.php
+++ Objects/View/ProjectView.php
@@ -8,45 +8,61 @@
 
 namespace View;
 
-
 class ProjectView
 {
     private $html = array();
     private $projects = array();
-    private $users = array();
-    private $client = null;
+    private $usersLead = array();
+    private $usersClient = null;
     private $displayValues = array();
     private $action;
     private $message;
 
-    function __construct($projects, $users, $client, $displayValues, $action, $message) {
+    function __construct($projects, $usersLead, $usersClient, $displayValues, $action, $message) {
         $this->projects = $projects;
-        $this->users = $users;
-        $this->client = $client;
+        $this->usersLead = $usersLead;
+        $this->usersClient = $usersClient;
         $this->displayValues = $displayValues;
         $this->action = $action;
         $this->message = $message;
     }
 
     function display() {
+
+        // Title and Message
         $this->html[] = "<h2>" . ucfirst($this->action) . " Project</h2>";
         $this->html[] = "<p>" . $this->message ."</p>";
-        $this->html[] = "<p><a href='index.php?page=project&action=create'>Create User</a></p>";
-        $this->html[] = "<p><a href='index.php?page=project&action=update'>Update User</a></p>";
-        $this->html[] = "<p><a href='index.php?page=project&action=delete'>Delete User</a></p>";
+
+        // Navigation links
+        $this->html[] = "<p><a href='index.php?page=project&action=create'>Create Project</a></p>";
+        $this->html[] = "<p><a href='index.php?page=project&action=update'>Update Project</a></p>";
+        $this->html[] = "<p><a href='index.php?page=project&action=delete'>Delete Project</a></p>";
+
+        // Project Form
         $this->html[] = '<form action ="index.php?page=project&action=' . $this->action .'" method="post">';
         if (($this->action == "create") || ($this->action == "update" &&  count($this->displayValues) > 0)) {
+
+            // Display project title and description fields
             $this->addField("text", "title", "Title:", (isset($this->displayValues['title']) ? $this->displayValues['title'] : null ));
             $this->addTextArea( "description", "Description:", (isset($this->displayValues['description']) ? $this->displayValues['description'] : null));
-            $this->addLead(isset($this->displayValues['email']) ? $this->displayValues['email'] : null);
-            // TODO add client
+
+            // Display project lead Options
+            $this->addUserInput("Lead", $this->usersLead, isset($this->displayValues['leadEmail']) ? $this->displayValues['leadEmail'] : null );
+
+            // Display project client options
+            $this->addUserInput("Client", $this->usersClient, isset($this->displayValues['clientEmail']) ? $this->displayValues['clientEmail'] : null );
+
+            // Submit button
             if ($this->action == "create") {
                 $this->html[] = '<br><br><input type="submit" value="Create Project"/>';
             } else {
                 $this->html[] = '<br><br><input type="submit" value="Update Project"/>';
             }
         } else {
+            // Project selection drop down selection
             $this->initialSelection();
+
+            // Submit button
             if ($this->action == "update") {
                 $this->html[] = '<br><br><input type="submit" value="Select User to Update"/>';
             } else {
@@ -78,28 +94,27 @@ class ProjectView
         $this->html[] = $input;
     }
 
-    function addLead($value) {
-        $this->html[] = '<label for="lead">Project Lead: </label>';
-        $this->html[] = '<select name = "lead" id="lead">';
+    function addUserInput($roleDescription, $value, $userList) {
+        $this->html[] = '<label for="user' . $roleDescription .'">Project Lead: </label>';
+        $this->html[] = '<select name = "user' . $roleDescription .'" id="user' . $roleDescription .'">';
 
-        for ($i=0; $i<count($this->users); $i++) {
-            $this->html[] = '<option value = "' . $this->users[$i]['email'] . '"' . ($value == $this->users[$i]['email'] ? " selected " : "") . '>' . $this->users[$i]['username'] . '</option>';
+        for ($i=0; $i<count($userList); $i++) {
+            $this->html[] = '<option value = "' . $userList[$i]['email'] . '"' . ($value == $userList[$i]['email'] ? " selected " : "") . '>' . $userList[$i]['username'] . '</option>';
         }
         $this->html[] = '</select>';
     }
 
     function initialSelection() {
-        $this->html[] = '<label for="email">Select User to ' . ucfirst($this->action) . ': </label>';
-        $select = '<select name = "email" id="email"';
+        $this->html[] = '<label for="projectID">Select Project to ' . ucfirst($this->action) . ': </label>';
+        $select = '<select name = "projectID" id="projectID"';
         if (count($this->projects) == 0) {
             $select .= ' disabled>';
         } else {
             $select .= '>';
         }
-        //$this->html[] = '<select name = "email" id="email">';
         $this->html[] = $select;
-        foreach ($this->projects as $user) {
-            $this->html[] = '<option value = "'. $user['email'] .'">Username: ' . $user['username'] . '  Role: ' . $user['role'] . '  Email: ' . $user['email'] .'</option>';
+        foreach ($this->projects as $project) {
+            $this->html[] = '<option value = "'. $project['projectID'] .'">Title: ' . $project['title'] . ' Project Lead: ' . $project['lead'] . '  Project Lead Email: ' . $project['leadEmail'] .'</option>';
         }
         $this->html[] = '</select>';
     }
